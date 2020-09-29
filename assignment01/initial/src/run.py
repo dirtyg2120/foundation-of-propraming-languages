b04c55c88a00ef996da869840cd95df92c150168
Hirito <hirito@pop-os.localdomain>
2020-09-29 17:32:51 +0700

    add os.environ to run code

--- assignment01/initial/src/run.py
+++ assignment01/initial/src/run.py
@@ -1,20 +1,25 @@
-import sys,os
-sys.path.append('./test/')
-import subprocess
-import unittest
 from antlr4 import *
+import unittest
+import subprocess
+import sys
+import os
+sys.path.append('./test/')
 
-#Make sure that ANTLR_JAR is set to antlr-4.8-complete.jar
+# Make sure that ANTLR_JAR is set to antlr-4.8-complete.jar
+os.environ['ANTLR_JAR'] = "/usr/local/lib/antlr-4.8-complete.jar"
 ANTLR_JAR = os.environ.get('ANTLR_JAR')
-TARGET = '../target/main/mc/parser' if os.name == 'posix' else os.path.normpath('../target/')
+TARGET = '../target/main/mc/parser' if os.name == 'posix' else os.path.normpath(
+    '../target/')
+
 
 def main(argv):
     if len(argv) < 1:
         printUsage()
     elif argv[0] == 'gen':
-        subprocess.run(["java","-jar",ANTLR_JAR,"-o","../target","-no-listener","-visitor","main/bkit/parser/BKIT.g4"])
+        subprocess.run(["java", "-jar", ANTLR_JAR, "-o", "../target",
+                        "-no-listener", "-visitor", "main/bkit/parser/BKIT.g4"])
     elif argv[0] == 'clean':
-        subprocess.run(["rm","-rf","../target/main"])
+        subprocess.run(["rm", "-rf", "../target/main"])
     elif argv[0] == 'test':
         if not './main/bkit/parser/' in sys.path:
             sys.path.append('./main/bkit/parser/')
@@ -34,7 +39,7 @@ def main(argv):
             printUsage()
     else:
         printUsage()
-    
+
 
 def test(suite):
     from pprint import pprint
@@ -48,10 +53,12 @@ def test(suite):
     stream.seek(0)
     print('Test output\n', stream.read())
 
+
 def printUsage():
     print("python3 run.py gen")
     print("python3 run.py test LexerSuite")
     print("python3 run.py test ParserSuite")
 
+
 if __name__ == "__main__":
-   main(sys.argv[1:])
+    main(sys.argv[1:])
