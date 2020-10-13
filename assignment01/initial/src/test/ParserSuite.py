dd28e2425c95c260efc714e6d1f406066e7c5cc3
hirito <hirito@pop-os.localdomain>
2020-10-13 15:02:19 +0700

    testing on statements

--- assignment01/initial/src/test/ParserSuite.py
+++ assignment01/initial/src/test/ParserSuite.py
@@ -15,31 +15,34 @@ class ParserSuite(unittest.TestCase):
         expect = "Error on line 1 col 5: ;"
         self.assertTrue(TestParser.checkParser(input, expect, 202))
 
-    # def test_01(self):
-    #     input = """ Var: x;
-    #                 Function: fact
-    #                     Parameter: n
-    #                     Body:
-    #                         **If n == 0 Then
-    #                             Return 1;
-    #                         Else
-    #                             Return n * fact (n - 1);
-    #                         EndIf.**
-    #                     EndBody.
+    def test_01(self):
+        input = """ Var: x;
+                    Function: fact
+                        Parameter: n
+                        Body:
+                            **If n == 0 Then
+                                Return 1;
+                            Else
+                                Return n * fact (n - 1);
+                            EndIf.**
+                        EndBody.
 
-    #                 Function: main
-    #                     Body:
-    #                         **x = 10;**
-    #                         fact (x);
-    #                     EndBody."""
-    #     expect = "successful"
-    #     self.assertTrue(TestParser.checkParser(input, expect, 1))
-
-    def test_02(self):
-        """For statement"""
-        input = """If bool_of_string ("True") Then
-                        a = int_of_string (read ());
-                        b = float_of_int (a) +. 2.0;
-                    EndIf."""
+                    Function: main
+                        Body:
+                            x = 10;
+                            fact (x);
+                        EndBody."""
         expect = "successful"
-        self.assertTrue(TestParser.checkParser(input, expect, 2))
+        self.assertTrue(TestParser.checkParser(input, expect, 1))
+
+    # def test_02(self):
+    #     """If statement"""
+    #     input = """If n == 0 Then Return 1; Else Return 2; EndIf."""
+    #     expect = "successful"
+    #     self.assertTrue(TestParser.checkParser(input, expect, 2))
+
+    def test_03(self):
+        """If statement"""
+        input = """If Then EndIf."""
+        expect = "successful"
+        self.assertTrue(TestParser.checkParser(input, expect, 3))
