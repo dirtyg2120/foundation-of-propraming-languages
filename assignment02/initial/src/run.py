62e84582b2bac44dd0cd052a35ad0ec03a945df7
hirito <trinhan194@gmail.com>
2020-11-10 19:41:15 +0700

    update

--- assignment02/initial/src/run.py
+++ assignment02/initial/src/run.py
@@ -1,24 +1,30 @@
-import sys,os
-sys.path.append('./test/')
-import subprocess
-import unittest
 from antlr4 import *
+import unittest
+import subprocess
+import sys
+import os
+sys.path.append('./test/')
 
-#Make sure that ANTLR_JAR is set to antlr-4.8-complete.jar
+# Make sure that ANTLR_JAR is set to antlr-4.8-complete.jar
+os.environ['ANTLR_JAR'] = "../../../lib/antlr-4.8-complete.jar"
 ANTLR_JAR = os.environ.get('ANTLR_JAR')
-TARGET = '../target/main/bkit/parser' if os.name == 'posix' else os.path.normpath('../target/')
-locpath = ['./main/bkit/parser/','./main/bkit/astgen/','./main/bkit/utils/']
+TARGET = '../target/main/bkit/parser' if os.name == 'posix' else os.path.normpath(
+    '../target/')
+locpath = ['./main/bkit/parser/', './main/bkit/astgen/', './main/bkit/utils/']
 for p in locpath:
     if not p in sys.path:
         sys.path.append(p)
+
+
 def main(argv):
     global ANTLR_JAR, TARGET
     if len(argv) < 1:
         printUsage()
     elif argv[0] == 'gen':
-        subprocess.run(["java","-jar",ANTLR_JAR,"-o","../target","-no-listener","-visitor","main/bkit/parser/BKIT.g4"])
+        subprocess.run(["java", "-jar", ANTLR_JAR, "-o", "../target",
+                        "-no-listener", "-visitor", "main/bkit/parser/BKIT.g4"])
     elif argv[0] == 'clean':
-        subprocess.run(["rm","-rf","../target/main"])
+        subprocess.run(["rm", "-rf", "../target/main"])
     elif argv[0] == 'test':
         if os.path.isdir(TARGET) and not TARGET in sys.path:
             sys.path.append(TARGET)
@@ -40,7 +46,7 @@ def main(argv):
             printUsage()
     else:
         printUsage()
-    
+
 
 def test(suite):
     from pprint import pprint
@@ -54,11 +60,13 @@ def test(suite):
     stream.seek(0)
     print('Test output\n', stream.read())
 
+
 def printUsage():
     print("python3 run.py gen")
     print("python3 run.py test LexerSuite")
     print("python3 run.py test ParserSuite")
     print("python3 run.py test ASTGenSuite")
 
+
 if __name__ == "__main__":
-   main(sys.argv[1:])
+    main(sys.argv[1:])
