86f0617259642b1d71f8ed76429b96e26febad43
hirito <trinhan194@gmail.com>
2020-11-11 20:23:53 +0700

    update

--- assignment01/initial/src/run.py
+++ assignment01/initial/src/run.py
@@ -6,7 +6,7 @@ import os
 sys.path.append('./test/')
 
 # Make sure that ANTLR_JAR is set to antlr-4.8-complete.jar
-os.environ['ANTLR_JAR'] = "../../../lib/antlr-4.8-complete.jar"
+os.environ['ANTLR_JAR'] = "../../../bin/antlr-4.8-complete.jar"
 ANTLR_JAR = os.environ.get('ANTLR_JAR')
 TARGET = '../target/main/mc/parser' if os.name == 'posix' else os.path.normpath(
     '../target/')
