ebe4eddff2102bf129fbaa59e269348df2f77d28
hirito <trinhan194@gmail.com>
2020-11-13 13:37:11 +0700

    first commit

---
+++ assignment02/initial/src/check_Parser.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+TESTCASES_DIR="./test/testcases/"
+SOLUTIONS_DIR="./test/solutions/"
+dir_lst=($TESTCASES_DIR
+    $SOLUTIONS_DIR)
+
+for dir in "${dir_lst[@]}"; do
+    # echo "$dir"
+    if [ ! -d "$dir" ]; then
+        echo "Directory $dir DOES NOT exists. Making one"
+        mkdir -p "$dir"
+    fi
+done
+
+python3 run.py gen
+# python3 run.py test LexerSuite
+python3 run.py test ParserSuite
+# python3 run.py test ASTGenSuite
