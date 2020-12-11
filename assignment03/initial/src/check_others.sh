1e6ff3be676a830ccca885d8970b7119cb7ac96c
edwardnguyen225 <trinhan194@gmail.com>
2020-12-11 16:20:25 +0700

    first add bash script

---
+++ assignment03/initial/src/check_others.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+TESTCASES_DIR="./test/testcases/"
+SOLUTIONS_DIR="./test/solutions/"
+dir_lst=($TESTCASES_DIR
+    $SOLUTIONS_DIR)
+
+for dir in "${dir_lst[@]}"; do
+    # echo "$dir"
+    if [ ! -d "$dir" ]; then
+        echo "Directory $dir DOES NOT exists. Making one"
+        mkdir -p "$dir"
+    fi
+done
+
+python3 run.py gen
+echo "=====================================================CHECK LEXER====================================================="
+python3 run.py test LexerSuite
+echo "=====================================================CHECK PARSER====================================================="
+python3 run.py test ParserSuite
+echo "=====================================================CHECK AST ====================================================="
+python3 run.py test ASTGenSuite
