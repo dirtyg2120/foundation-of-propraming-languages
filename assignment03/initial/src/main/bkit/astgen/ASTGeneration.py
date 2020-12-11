0d68f73999902dc7ac6daa2e55ac272756630d72
edwardnguyen225 <trinhan194@gmail.com>
2020-12-11 16:21:18 +0700

    update from assignment 2

--- assignment03/initial/src/main/bkit/astgen/ASTGeneration.py
+++ assignment03/initial/src/main/bkit/astgen/ASTGeneration.py
@@ -1,10 +1,211 @@
+from main.bkit.utils.AST import *
 from BKITVisitor import BKITVisitor
 from BKITParser import BKITParser
 from AST import *
+# from AST_copy import *
+from functools import reduce
+
 
 class ASTGeneration(BKITVisitor):
-    def visitProgram(self,ctx:BKITParser.ProgramContext):
-        return Program([VarDecl(Id(ctx.ID().getText()),[],None)])
+    def visitProgram(self, ctx: BKITParser.ProgramContext):
+        # return Program([VarDecl(Id(ctx.ID().getText()), [], None)])
+        var_decl_lst = flatten([element.accept(self)
+                                for element in ctx.variable_declaration()])  # cần flatten lại cái thằng này
+        func_decl = [e.accept(self) for e in ctx.func_declaration()]
+        return Program(var_decl_lst + func_decl)
 
-    
+    def visitVariable_declaration(self, ctx: BKITParser.Variable_declarationContext):
+        return ctx.variable_list().accept(self)
 
+    def visitVariable_list(self, ctx):
+        # dễ tạo list trong list
+        return [e.accept(self) for e in ctx.variable()]
+
+    def visitVariable(self, ctx):
+        varDimen_lst = [int(e.getText())
+                        for e in ctx.INT()] if ctx.INT() else []
+        varInit = ctx.exp().accept(self) if ctx.exp() else None
+        return VarDecl(Id(ctx.ID().getText()), varDimen_lst, varInit)
+
+    def visitFunc_declaration(self, ctx):
+        param = ctx.param_declaration().accept(self) if ctx.param_declaration() else []
+        body = ctx.body().accept(self)
+        return FuncDecl(Id(ctx.ID().getText()), param, body)
+
+    def visitParam_declaration(self, ctx):
+        return ctx.param_list().accept(self)
+
+    def visitParam_list(self, ctx):
+        return [e.accept(self) for e in ctx.param()]
+
+    def visitParam(self, ctx):
+        varDimen_lst = [int(e.getText())
+                        for e in ctx.INT()] if ctx.INT() else []
+        return VarDecl(Id(ctx.ID().getText()), varDimen_lst, None)
+
+    def visitBody(self, ctx):
+        mini_body = ctx.mini_body().accept(self)
+        return (mini_body[0], mini_body[1])
+
+    def visitMini_body(self, ctx):
+        var_decl_lst = []
+        stmt_lst = []
+        if ctx.variable_declaration():
+            var_decl_lst = flatten([element.accept(self)
+                                    for element in ctx.variable_declaration()])
+        if ctx.stmt():
+            stmt_lst = [element.accept(self) for element in ctx.stmt()]
+        return [var_decl_lst, stmt_lst]
+
+    def visitStmt(self, ctx):
+        return ctx.getChild(0).accept(self)
+
+    def visitStatement_assign(self, ctx):
+        lhs = Id(ctx.ID().getText()) if ctx.ID(
+        ) else ctx.array_cell_decl().accept(self)
+        rhs = ctx.exp().accept(self)
+        return Assign(lhs, rhs)
+
+    def visitArray_cell_decl(self, ctx):
+        arr = Id(ctx.ID().getText()) if ctx.ID(
+        ) else ctx.function_call().accept(self)
+        exp_lst = [e.accept(self) for e in ctx.exp()]
+        return ArrayCell(arr, exp_lst)
+
+    def visitStatement_if(self, ctx):
+        ifThenStmt_lst = [ctx.if_then_stmt().accept(self)]
+        if ctx.else_if_stmt():
+            ifThenStmt_lst = ifThenStmt_lst + \
+                [e.accept(self) for e in ctx.else_if_stmt()]
+        elseStmt = (ctx.else_stmt().accept(self)
+                    ) if ctx.else_stmt() else ([], [])
+        return If(ifThenStmt_lst, elseStmt)
+
+    def visitIf_then_stmt(self, ctx):
+        expr = ctx.exp().accept(self)
+        then_stmt = ctx.mini_body().accept(self)
+        return (expr, then_stmt[0], then_stmt[1])
+
+    def visitElse_if_stmt(self, ctx):
+        expr = ctx.exp().accept(self)
+        then_stmt = ctx.mini_body().accept(self)
+        return (expr, then_stmt[0], then_stmt[1])
+
+    def visitElse_stmt(self, ctx):
+        then_stmt = ctx.mini_body().accept(self)
+        return (then_stmt[0], then_stmt[1])
+
+    def visitStatement_for(self, ctx):
+        mini_body = ctx.mini_body().accept(self)
+        loop = (mini_body[0], mini_body[1])
+        return For(Id(ctx.ID().getText()), ctx.exp(0).accept(self), ctx.exp(1).accept(self), ctx.exp(2).accept(self), loop)
+
+    def visitStatement_while(self, ctx):
+        expr = ctx.exp().accept(self)
+        mini_body = ctx.mini_body().accept(self)
+
+        sl = (mini_body[0], mini_body[1])
+        return While(expr, sl)
+
+    def visitStatement_do_while(self, ctx):
+        mini_body = ctx.mini_body().accept(self)
+        sl = (mini_body[0], mini_body[1])
+        expr = ctx.exp().accept(self)
+        return Dowhile(sl, expr)
+
+    def visitStatement_break(self, ctx):
+        return Break()
+
+    def visitStatement_continue(self, ctx):
+        return Continue()
+
+    def visitStatement_call(self, ctx):
+        func_call = ctx.function_call().accept(self)
+        return CallStmt(func_call.method, func_call.param)
+
+    def visitStatement_return(self, ctx):
+        expr = ctx.exp().accept(self) if ctx.exp() else None
+        return Return(expr)
+
+    def visitFunction_call(self, ctx):
+        method = Id(ctx.ID().getText())
+        param = [e.accept(self) for e in ctx.exp()]
+        return CallExpr(method, param)
+
+    def visitExp(self, ctx):
+        if ctx.getChildCount() == 1:
+            return ctx.exp1(0).accept(self)
+        op = ctx.getChild(1).getText()
+        left = ctx.exp1(0).accept(self)
+        right = ctx.exp1(1).accept(self)
+        return BinaryOp(op, left, right)
+
+    def visitExp1(self, ctx):
+        if ctx.getChildCount() == 1:
+            return ctx.exp2().accept(self)
+        op = ctx.getChild(1).getText()
+        left = ctx.exp1().accept(self)
+        right = ctx.exp2().accept(self)
+        return BinaryOp(op, left, right)
+
+    def visitExp2(self, ctx):
+        if ctx.getChildCount() == 1:
+            return ctx.exp3().accept(self)
+        op = ctx.getChild(1).getText()
+        left = ctx.exp2().accept(self)
+        right = ctx.exp3().accept(self)
+        return BinaryOp(op, left, right)
+
+    def visitExp3(self, ctx):
+        if ctx.getChildCount() == 1:
+            return ctx.exp4().accept(self)
+        op = ctx.getChild(1).getText()
+        left = ctx.exp3().accept(self)
+        right = ctx.exp4().accept(self)
+        return BinaryOp(op, left, right)
+
+    def visitExp4(self, ctx):
+        if ctx.getChildCount() == 1:
+            return ctx.exp5().accept(self)
+        op = ctx.getChild(0).getText()
+        body = ctx.exp4().accept(self)
+        return UnaryOp(op, body)
+
+    def visitExp5(self, ctx):
+        if ctx.getChildCount() == 1:
+            return ctx.operand().accept(self)
+        op = ctx.getChild(0).getText()
+        body = ctx.exp5().accept(self)
+        return UnaryOp(op, body)
+
+    def visitOperand(self, ctx):
+        if ctx.getChildCount() == 3:
+            return ctx.exp().accept(self)
+        elif ctx.ID():
+            return Id(ctx.ID().getText())
+        return ctx.getChild(0).accept(self)
+
+    def visitLiteral(self, ctx):
+        if ctx.INT():
+            _int = ctx.INT().getText()
+            if "o" in _int or "O" in _int:
+                return IntLiteral(int(_int,8))
+            elif "x" in _int or "X" in _int:
+                return IntLiteral(int(_int,16))
+            else:
+                return IntLiteral(int(_int))
+        elif ctx.FLOAT():
+            return FloatLiteral(float(ctx.FLOAT().getText()))
+        elif ctx.BOOLEAN():
+            return BooleanLiteral(bool(ctx.BOOLEAN().getText() == 'True'))
+        elif ctx.STRING():
+            return StringLiteral(str(ctx.STRING().getText()))
+        return ctx.getChild(0).accept(self)
+
+    def visitArray_literal(self, ctx):
+        value = [e.accept(self) for e in ctx.exp()]
+        return ArrayLiteral(value)
+
+
+def flatten(lst):
+    return list(reduce(lambda x, y: x+y, lst, []))
--- assignment03/initial/src/main/bkit/parser/BKIT.g4
+++ assignment03/initial/src/main/bkit/parser/BKIT.g4
@@ -20,24 +20,226 @@ def emit(self):
         return result;
 }
 
-options{
-	language=Python3;
+options {
+	language = Python3;
 }
 
-program  : VAR COLON ID SEMI EOF ;
+/*====================================================== PROGRAM STRUCTURE ====================================================== */
+program: variable_declaration* func_declaration* EOF;
 
-ID: [a-z]+ ;
+variable_declaration: VAR COLON variable_list SM;
+variable_list: variable (CM variable)*;
+variable: ID (LB_SQUARE INT RB_SQUARE)* (ASSIGN exp)?;
 
-SEMI: ';' ;
+func_declaration: FUNCTION COLON ID param_declaration? body;
+param_declaration: PARAMETER COLON param_list;
+param_list: param (CM param)*;
+param: ID (LB_SQUARE INT RB_SQUARE)*;
 
-COLON: ':' ;
+body: begin_body mini_body end_body;
 
-VAR: 'Var' ;
+/*========== Statements ==========*/
+stmt:
+	statement_assign
+	| statement_if
+	| statement_for
+	| statement_while
+	| statement_do_while
+	| statement_break
+	| statement_continue
+	| statement_call
+	| statement_return;
 
-WS : [ \t\r\n]+ -> skip ; // skip spaces, tabs, newlines
+// statement_assign: ID (LB_SQUARE exp RB_SQUARE)* ASSIGN exp SM;
+statement_assign: ( ID | array_cell_decl) ASSIGN exp SM;
 
+array_cell_decl: (ID | function_call) (LB_SQUARE exp RB_SQUARE)+;
+mini_body: variable_declaration* stmt*;
+
+statement_if: if_then_stmt else_if_stmt* else_stmt? ENDIF DOT;
+if_then_stmt: IF exp THEN mini_body;
+else_if_stmt: ELSEIF exp THEN mini_body;
+else_stmt: ELSE mini_body;
+
+// statement_if: IF THEN ENDIF DOT;
+
+statement_for:
+	FOR LB_ROUND ID ASSIGN exp CM exp CM exp RB_ROUND DO mini_body ENDFOR DOT;
+
+statement_while: WHILE exp DO mini_body ENDWHILE DOT;
+
+statement_do_while: DO mini_body WHILE exp ENDDO DOT;
+
+statement_break: BREAK SM;
+statement_continue: CONTINUE SM;
+statement_call: function_call SM;
+function_call: ID LB_ROUND (exp (CM exp)*)? RB_ROUND;
+
+statement_return: RETURN exp? SM;
+
+exp: exp1 ( OP_COMPARE_INT | OP_COMPARE_FLOAT) exp1 | exp1;
+exp1: exp1 (AND | OR) exp2 | exp2;
+exp2:
+	exp2 (ADD_INT | ADD_FLOAT | SUB_INT | SUB_FLOAT) exp3
+	| exp3;
+exp3:
+	exp3 (MUL_INT | MUL_FLOAT | DIV_INT | DIV_FLOAT | MOD_INT) exp4
+	| exp4;
+exp4: NOT exp4 | exp5;
+exp5: (SUB_FLOAT | SUB_INT) exp5 | operand;
+
+operand: literal | ID | function_call | LB_ROUND exp RB_ROUND;
+
+literal:
+	INT
+	| FLOAT
+	| BOOLEAN
+	| array_literal
+	| STRING
+	| array_cell_decl;
+
+array_literal: LB_CURLY ( exp (CM exp)*)? RB_CURLY;
+
+begin_body: BODY COLON;
+end_body: ENDBODY DOT;
+
+/*================================================================
+ LEXER ================================================================
+ */
+
+INT: DECIMAL | HEX | OCTAL;
+fragment DECIMAL: DIGIT | [1-9]DIGIT*;
+fragment HEX: '0' [xX] (DIGIT | [A-F])*;
+fragment OCTAL: '0' [oO][0-7]*;
+
+FLOAT: INT (DOT DIGIT* EXPONENT* | EXPONENT);
+fragment EXPONENT: [eE][+-]? DIGIT+;
+
+BOOLEAN: TRUE | FALSE;
+
+STRING:
+	'"' CHAR_LITERAL_IN_STRING* '"' { 
+    value = str(self.text)
+    self.text = value[1:-1]
+};
+
+// ARRAY: LB_CURLY WS* (WS* LITERALS WS* (CM WS* LITERALS WS*)*)? WS* RB_CURLY;
+
+fragment LITERALS: INT | FLOAT | BOOLEAN | STRING;
+COMMENT: '**' .*? '**' -> skip; //** **
+
+/*========== identifiers ==========*/
+ID: LOWER_LETTER ('_' | LOWER_LETTER | UPPER_LETTER | DIGIT)*;
+
+/*========== keywords ==========*/
+VAR: 'Var';
+FUNCTION: 'Function';
+PARAMETER: 'Parameter';
+
+BODY: 'Body';
+ENDBODY: 'EndBody';
+
+IF: 'If';
+THEN: 'Then';
+ELSEIF: 'ElseIf';
+ELSE: 'Else';
+ENDIF: 'EndIf';
+
+FOR: 'For';
+ENDFOR: 'EndFor';
+
+WHILE: 'While';
+DO: 'Do';
+ENDWHILE: 'EndWhile';
+ENDDO: 'EndDo';
+
+BREAK: 'Break';
+CONTINUE: 'Continue';
+RETURN: 'Return';
+
+TRUE: 'True';
+FALSE: 'False';
+
+/*========= operators ==========*/
+ASSIGN: '=';
+
+// INTEGER
+ADD_INT: '+';
+SUB_INT: '-';
+MUL_INT: '*';
+DIV_INT: '\\';
+MOD_INT: '%';
+
+OP_COMPARE_INT:
+	EQUAL
+	| NOT_EQUAL_INT
+	| LT_INT
+	| GT_INT
+	| LTE_INT
+	| GTE_INT;
+EQUAL: '==';
+NOT_EQUAL_INT: '!=';
+LT_INT: '<';
+GT_INT: '>';
+LTE_INT: '<=';
+GTE_INT: '>=';
+
+// FLOAT
+ADD_FLOAT: '+.';
+SUB_FLOAT: '-.';
+MUL_FLOAT: '*.';
+DIV_FLOAT: '\\.';
+
+OP_COMPARE_FLOAT:
+	NOT_EQUAL_FLOAT
+	| LT_FLOAT
+	| GT_FLOAT
+	| LTE_FLOAT
+	| GTE_FLOAT;
+NOT_EQUAL_FLOAT: '=/=';
+LT_FLOAT: '<.';
+GT_FLOAT: '>.';
+LTE_FLOAT: '<=.';
+GTE_FLOAT: '>=.';
+
+// BOOLEAN
+NOT: '!';
+AND: '&&';
+OR: '||';
+
+/*========== seperators ==========*/
+LB_ROUND: '(';
+RB_ROUND: ')';
+LB_SQUARE: '[';
+RB_SQUARE: ']';
+LB_CURLY: '{';
+RB_CURLY: '}';
+COLON: ':';
+SM: ';';
+DOT: '.';
+CM: ',';
+
+WS: [ \t\r\n]+ -> skip; // skip spaces, tabs, newlines
+
+fragment DIGIT: [0-9];
+fragment UPPER_LETTER: [A-Z];
+fragment LOWER_LETTER: [a-z];
+
+fragment CHAR_LITERAL_IN_STRING: (
+		'\\' [btnfr'\\]
+		| '\'"'
+		| ~[\n\\"']
+	);
 
 ERROR_CHAR: .;
-UNCLOSE_STRING: .;
-ILLEGAL_ESCAPE: .;
-UNTERMINATED_COMMENT: .;
+UNCLOSE_STRING:
+	'"' CHAR_LITERAL_IN_STRING* {
+        value = str(self.text)
+        self.text = value[1:]
+    };
+ILLEGAL_ESCAPE:
+	'"' CHAR_LITERAL_IN_STRING* ('\\' ~[btnfr'\\] | '\'' ~["]) {
+        value = str(self.text)
+        self.text = value[1:]
+    };
+UNTERMINATED_COMMENT: '**' .*?;
